bc488082a726ff48671342c5ff3c03dc60f2d9fe
Andrei <andreikagit@users.noreply.github.com>
2021-12-17 23:13:03 +0200

    keep the old files

---
+++ old_step3_create_hellen72.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../../hellen-one
+sh ./bin/create_board.sh "../commercial_products/hellen72" "72" "d" "bom_replace_72-d.csv"
--- step3_create_hellen72.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../../hellen-one
-sh ./bin/create_board.sh "../commerial_products/hellen72" "72" "d" "bom_replace_72-d.csv"
