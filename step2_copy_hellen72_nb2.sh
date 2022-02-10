e2114d19abc9f63b4b66524070451ca73c3105cc
andreika-git <andrei.home@gmail.com>
2022-02-10 21:16:56 +0200

    update

--- step2_copy_hellen72_nb2.sh
+++ step2_copy_hellen72_nb2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "72_NB2" "e"
+python2.7 ./bin/copy_from_Kicad.py "frames:hellen" ".." "../../gerber" "72_NB2" "e"
 
 echo "Done!"
